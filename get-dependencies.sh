0719be980aed4e631c9113cd0c46866d74d2ceb3
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-04-17 19:22:38 -0400

    make dedicated script for dependencies

---
+++ get-dependencies.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+set -ex
+
+sed -i 's/DownloadUser/#DownloadUser/g' /etc/pacman.conf
+
+if [ "$(uname -m)" = 'x86_64' ]; then
+	PKG_TYPE='x86_64.pkg.tar.zst'
+else
+	PKG_TYPE='aarch64.pkg.tar.xz'
+fi
+
+LLVM_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/llvm-libs-nano-$PKG_TYPE"
+LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
+OPUS_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/opus-nano-$PKG_TYPE"
+
+echo "Installing build dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm \
+	base-devel \
+	curl \
+	desktop-file-utils \
+	git \
+	llvm \
+	mesa \
+	patchelf \
+	ppsspp \
+	strace \
+	vulkan-intel \
+	vulkan-nouveau \
+	vulkan-radeon \
+	wget \
+	xorg-server-xvfb \
+	zsync
+
+echo "Installing debloated pckages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$LLVM_URL" -O   ./llvm-libs.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$LIBXML_URL" -O ./libxml2.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$OPUS_URL" -O   ./opus-nano.pkg.tar.zst
+
+pacman -U --noconfirm ./*.pkg.tar.zst
+rm -f ./*.pkg.tar.zst
+
+echo "All done!"
+echo "---------------------------------------------------------------"
